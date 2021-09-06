b94a4e7587b04870eb8b652192fd83d4adad44b4
shiva <shivadatha07@gmail.com>
2021-09-06 23:27:09 +0530

    added model.config

--- api/main-tf.py
+++ api/main-tf.py
@@ -4,10 +4,11 @@ import numpy as np
 from io import BytesIO
 from PIL import Image
 import tensorflow as tf
+import requests
 
 app = FastAPI()
 
-MODEL = tf.keras.models.load_model("../saved_models/1")
+endpoint = "http://localhost:8501/v1/models/potatoes_model:predict"
 
 CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
 
@@ -25,11 +26,17 @@ async def predict(
 ):
     image = read_file_as_image(await file.read())
     img_batch = np.expand_dims(image, 0)
-    
-    predictions = MODEL.predict(img_batch)
 
-    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
-    confidence = np.max(predictions[0])
+    json_data = {
+        "instances": img_batch.tolist()
+    }
+    
+    response = requests.post(endpoint, json=json_data)
+
+    prediction = response.json()["predictions"][0]
+
+    predicted_class = np.argmax(prediction)
+    confidence = np.max(predictions)
     return {
         'class': predicted_class,
         'confidence': float(confidence)
@@ -37,3 +44,7 @@ async def predict(
 
 if __name__ == "__main__":
     uvicorn.run(app, host='localhost', port=8000)
+
+
+
+docker run -t --rm -p 8501:8501 -v /home/shiva/Desktop/Github-Uploads/pdc/Potato-Disease-Classification-Using-CNN:/potato-disease-classification tensorflow/serving --rest_api_port=8501 --model_config_file = /potato-disease-classification/models.config
